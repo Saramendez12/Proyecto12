5861ab625ef63071db71918a973fd45291b2880b
Sara Mendez <110432454+Saramendez12@users.noreply.github.com>
2022-11-11 18:19:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.graph_objs as go
 
 pca_3=pd.read_csv("./pca_3.csv")
+tabla=pd.read_csv("./tabla.csv")
 
 st.title("Proyecto 12")
 st.write(" En la presente pagina, se presenta una presentacion de un indice de felicidad constriudo a partir de 10 diferentes variables de 60 paises ")
@@ -27,6 +28,14 @@ st.subheader("Análisis descriptivo y gráfico")
 st.subheader("Conclusión General")
 st.write("[Link al Notebook](https://colab.research.google.com/drive/115jkwsUACKRFmJAgcehE8PxzRzFha0Y1?usp=sharing)")
 
+X = tabla.to_numpy()
+scal = StandardScaler()
+X_scal = scal.fit_transform(tabla)
+pca = PCA(n_components=10)
+X_pca = pca.fit_transform(X_scal)
+
+kmeans = KMeans(n_clusters=3, random_state=777,algorithm='elkan').fit(X_scal)
+pca_3['labels'] = kmeans.labels_
 Scene = dict(xaxis = dict(title  = 'PCA1'),yaxis = dict(title  = 'PCA2'),zaxis = dict(title  = 'PCA3'))
 labels = kmeans.labels_
 trace = go.Scatter3d(x=pca_3['PCA1'], y=pca_3['PCA2'], z=pca_3['PCA3'], mode='markers',marker=dict(color = labels, size= 10, line=dict(color= 'black',width = 10)))
