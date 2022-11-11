088448db88642f30da5aa560c16a186c38ef563f
Sara Mendez <110432454+Saramendez12@users.noreply.github.com>
2022-11-11 17:18:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,20 @@
-!pip install wbgapi
-
+import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import wbgapi as wb
-from sklearn.linear_model import LinearRegression
 
-from scipy import linalg
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-from sklearn.manifold import TSNE
-from sklearn.cluster import KMeans
-from mpl_toolkits.mplot3d import Axes3D
-import plotly.graph_objs as go
-from plotly import tools
-from plotly.subplots import make_subplots
-import plotly.offline as py
+st.title("Proyecto 12")
+st.write(" En la presente pagina, se presenta una presentacion de un indice de felicidad constriudo a partir de 10 diferentes variables de 60 paises ")
+st.subheader("Explicacion variables usadas: ")
+st.write("1 Inflacion:")
+st.write("2 IDH:")
+st.write("3 PIB per Capita:")
+st.write("4 :")
+st.write("5 Tasa de suicidios:")
+st.write("6 Salario medio de los empleados:")
+st.write("7 Desempleo total:")
+st.write("8 Esperanza de vida:")
+st.write("9 Poblacion mayor a los 65 años:")
+st.write("10 Tasa de homicidios intencionales:")
 
-tabla=wb.data.DataFrame(["FP.CPI.TOTL.ZG","HD.HCI.OVRL","NY.GDP.PCAP.CD","SE.XPD.TOTL.GD.ZS","SH.STA.SUIC.P5",
-                         "SL.EMP.WORK.ZS","SL.UEM.TOTL.ZS","SP.DYN.LE00.IN","SP.POP.65UP.TO.ZS","VC.IHR.PSRC.P5"],
-                        ["ALB","DEU","ARG","ARM","AUS","AUT","BGD","BOL","BRA","BTN","CAN","CHN","COL","KOR","CRI",
-                         "DNK","SLV","ARE","ESP","USA","EST","FJI","FIN","FRA","GRC","IND","IDN","ISL","IRL","ISR",
-                         "JAM","JPN","LBN","MYS","MAR","MEX","PAK","PER","URY","POL","QAT","GBR","DOM","SRB","ZAF",
-                         "CHE","THA","TZA","TUN","TUR","UKR","YEM","AGO","SYR","SGP","RUS","SDN","KAZ","PNG","NLD"],range(2017,2018))
-tabla.rename(columns={'FP.CPI.TOTL.ZG':"Inflacion_A", 'HD.HCI.OVRL':"IDH", 
-                      'NY.GDP.PCAP.CD':"PIB_perca",'SE.XPD.TOTL.GD.ZS':"GPEDU_PIB",
-                      'SH.STA.SUIC.P5':"Suicidio_rate", 'SL.EMP.WORK.ZS':"Salario empleados", 
-                      'SL.UEM.TOTL.ZS':"Desempleo_total", 'SP.DYN.LE00.IN':"Esperanza_vida",
-                      'SP.POP.65UP.TO.ZS':"POB_+65", 'VC.IHR.PSRC.P5':"Homicidios_inte"},inplace=True)
-tabla
+st.subheader("Reduccion de dimencionalidad (PCA): ")
+st.subheader("Cluster y Kmeans:")
