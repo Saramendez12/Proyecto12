85a2c8957de43b37b1dc18eaaa1fa400ce6dd86c
IanDam <111082036+IanDam@users.noreply.github.com>
2022-11-11 22:37:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,7 @@ st.subheader("Conclusión General")
 st.write("[Link al Notebook](https://colab.research.google.com/drive/115jkwsUACKRFmJAgcehE8PxzRzFha0Y1?usp=sharing)")
 
 
-trace = go.Scatter3d(x=pca_3['PCA1'], y=pca_3['PCA2'], z=pca_3['PCA3'], mode='markers',marker=dict(color = labels, size= 10, line=dict(color= 'black',width = 10)))
-layout = go.Layout(margin=dict(l=0,r=0),scene = Scene,height = 800,width = 800)
-data = [trace]
-fig = go.Figure(data = data, layout = layout)
-fig.show()
+
 
 trace = go.Scatter3d(x=pca_3['PCA1'], y=pca_3['PCA2'], z=pca_3['PCA3'], mode='markers',marker=dict(color = 'green', size= 10, line=dict(color= 'black',width = 10)))
 layout = go.Layout(margin=dict(l=0,r=0),scene = Scene,height = 800,width = 800)
