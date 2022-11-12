71c8b9cfda3de938e57ac6911c066fa816241bf6
Sara Mendez <110432454+Saramendez12@users.noreply.github.com>
2022-11-12 17:40:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -90,16 +90,15 @@ st.plotly_chart(fig,use_container_widht=True)
 st.caption("Grafica interactiva de PCA")
 
 st.write("Para poder graficar y ver como se explica la varianza a traves de los componentes principales seleccionados, realizamos a partir del pca_3 (dataframe de los PCA)"
-          "la creación de la varianza explicada y acumulada, en el cual se ve de forma clara que el numero de componentes elegido (3) es el que puede de mejor manera"
-          "explicar la varianza")
-st.image("./Imagenes12/VAR.png")
+          "la creación de la varianza explicada y acumulada, en el cual se ve de forma clara que el numero de componentes elegido (3) es el que puede de mejor manera explicar la varianza")
+st.image("./Imagenes12/VAR.PNG")
 
 st.subheader("Calificación Davies-Bouldin:")
 st.write("Para analizar el numero de clusters optimos realizamos el grafico del codo, con un rango de 2 a 10. En este se busca identificar" 
 "la cantidad optima de clusters que minimizen la puntación de Davies Bouldin. A pesar de en que este grafico las medidas no se llevan mucha"
 "diferencia solo de 1 en 1 , se ve claramente que el valor minimo es 3, por lo tanto se concluye que la puntuación de Davies Bouldin se minimiza"
          "con 3 grupos o clusters y se puede considera este k means. ")
-st.image("./Imagenes12/DB.png")
+st.image("./Imagenes12/DB.PNG")
 st.write("Por ultimo la calificación o puntaje de Davies Bouldin es de 1.034 aproximadamente el cual se consigue con el número de grupos ya elegido.")
 
 st.subheader("Algoritmo de Clustering seleccionado K means:")
